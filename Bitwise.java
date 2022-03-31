6b0d1c7a11e0ab217e310e920e950950450f1995
shubhamshriwastwa <shubhsrivastava110@gmail.com>
2022-03-31 22:48:56 +0530

    this is vowel or not

--- Bitwise.java
+++ Bitwise.java
@@ -6,11 +6,18 @@ public static void main(String[] args){
     System.out.println(2 & 3); 
      System.out.println(2 | 3); 
       System.out.println(2 ^ 3); 
-       System.out.println(~2); 
-        System.out.println(34<<1);
-       System.out.println(34>>2); 
-        System.out.println(-4<<1); 
-         System.out.println(-4>>2);
+    System.out.println(~2); 
+    System.out.println(34<<1);
+    System.out.println(34>>2); 
+    System.out.println(-4<<1); 
+    System.out.println(-4>>2);
+
     
 } 
 }
+
+
+
+
+
+
