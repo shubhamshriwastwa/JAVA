276e88c55c6760e2c41a94c0e0fbc5c84a9487ca
shubhamshriwastwa <shubhsrivastava110@gmail.com>
2022-04-06 22:21:18 +0530

    Opretors Arithmetic in java

---
+++ Array_Sort.java
@@ -0,0 +1,12 @@
+public class Array_Sort{
+    public static void main(String args[]){ 
+        int arr[] = new int[] { 2,3,4,5,1,};
+        Arrays.sort(arr);  
+        System.out.println("array are sorted")
+        for(int i=0;i<arr.length;i++){ 
+            
+
+        }
+
+    }
+}
