6624b1519a9bae7897257c98847a12f6159183aa
shubhamshriwastwa <shubhsrivastava110@gmail.com>
2022-05-26 11:36:33 +0530

    method

---
+++ StringMethods.java
@@ -0,0 +1,9 @@
+public class StringMethods {
+    public static void main(String[] args) {
+        String str = "shubham";
+
+        str = str.toUpperCase();
+        System.out.println(str);
+    }
+
+}
