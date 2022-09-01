ef7eb59dde3966542d42941c073969103a7670b0
shubhamshriwastwa <shubhsrivastava110@gmail.com>
2022-09-01 20:04:35 +0530

    Print1to5

---
+++ web devlopment/Condition_statment/Recusion/Print_Num.java
@@ -0,0 +1,19 @@
+package Recusion;
+
+public class Print_Num {
+    public static void Print_Numb(int n) {
+        if (n == 5) {
+            return;
+        }
+
+        System.out.println(n);
+        Print_Numb(n + 1);
+
+    }
+
+    public static void main(String[] args) {
+        int n = 1;
+        Print_Numb(n);
+    }
+
+}
