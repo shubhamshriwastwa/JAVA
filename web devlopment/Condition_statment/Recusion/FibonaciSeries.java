5c7dee1fe564e247cc39be5394481a25e45e701a
shubhamshriwastwa <shubhsrivastava110@gmail.com>
2022-09-05 21:35:05 +0530

    FibonaciSeries

---
+++ web devlopment/Condition_statment/Recusion/FibonaciSeries.java
@@ -0,0 +1,25 @@
+package Recusion;
+
+public class FibonaciSeries {
+
+    public static void Printfib(int a, int b, int n) {
+        if (n == 0) {
+            return;
+        }
+        int c = a + b;
+        System.out.println(c);
+        Printfib(b, c, n - 1);
+
+    }
+
+    public static void main(String args) {
+        int a = 0;
+        int b = 1;
+        System.out.println(a);
+        System.out.println(b);
+        int n = 7;
+        Printfib(a, b, n - 2);
+
+    }
+
+}
