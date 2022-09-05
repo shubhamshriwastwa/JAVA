96562fde6158c6dadcf444cdd5de62e97d8c7dc3
shubhamshriwastwa <shubhsrivastava110@gmail.com>
2022-09-05 19:38:13 +0530

    Sum of N natural number

---
+++ .vscode/Recurion1/Sum_Natural.java
@@ -0,0 +1,19 @@
+public class Sum_Natural {
+    public static void printSum(int i, int n, int sum) {
+        if (i == n) {
+            sum += i;
+            System.out.println(sum);
+            return;
+
+        }
+
+        sum += i;
+        printSum(i + 1, n, sum);
+    }
+
+    public static void main(String args) {
+        printSum(1, 5, 0);
+
+    }
+
+}
