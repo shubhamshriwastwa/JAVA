ea6d2ecf65a4327f2e9ea723b183eb379bdd1779
shubhamshriwastwa <shubhsrivastava110@gmail.com>
2022-09-15 22:36:05 +0530

    Arrray in java

---
+++ Array/Array1.java
@@ -0,0 +1,14 @@
+public class Array1 {
+    public static void main(String args[]) {
+        int[] marks = new int[5];
+        marks[0] = 76;
+        marks[1] = 87;
+        marks[2] = 45;
+        marks[3] = 34;
+        System.out.println(marks[0]);
+        System.out.println(marks[2]);
+        System.out.println(marks[3]);
+        System.out.println(marks[4]);
+
+    }
+}
